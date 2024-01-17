f606544a058ff07cabdc8ba964447d512dfd86c9
kotesh-arya <koteswarraomudila@gmail.com>
2024-01-17 11:50:24 +0530

    ADD: HTML Form and it's input elements and their attributes

--- index.js
+++ index.js
@@ -14,8 +14,23 @@ let obj = {
   city: "jammalamadugu",
 };
 
-obj.forEach((item) => {
-  console.log(item);  //  Uncaught TypeError: obj.forEach is not a function, cause the obj donot have any methdod with the name of forEach
+// obj.forEach((item) => {
+//   console.log(item); //  Uncaught TypeError: obj.forEach is not a function, cause the obj donot have any methdod with the name of forEach
+// });
+
+const formElement = document.getElementById("my-form");
+let formData = {};
+console.log(formElement.length, "elements length");
+const submitBtn = document.getElementById("form-submit-btn");
+function handleFormSubmission(e) {
+  for (let i = 0; i < formElement.length; i++) {
+    if (formElement[i].type !== "submit") {
+      formData[formElement[i].name] = formElement[i].value;
+    }
+  }
+  console.log(formData);
+}
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleFormSubmission();
 });
-
-
